6dc9afee288ce2fafffa7ffca21904e3a7104596
root <root@RedhatLinux.1tg3lq3xtbqexozclv1qyw1c0h.bx.internal.cloudapp.net>
2019-05-14 07:43:57 +0000

    User and password auto making script

--- userdefine.sh
+++ userdefine.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-read -p "please enter username:" UserName
-useradd -m $UserName
-read -p -s "Enter password:" Password
-passwd -p $Password
-echo $Password
-echo $UserName
+#set -x -e
+read -p "Please enter the user name:" USER_NAME
+PASSWORD=$(curl -sL https://helloacm.com/api/random/?n=14)
+#read -s -p "Please enter the password:" USER_PASS
+#useradd -m ${USER_NAME} -p ${PASSWORD}
+useradd -m ${USER_NAME}
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+echo "The Username is ${USER_NAME} & Password is ${PASSWORD}"
 
